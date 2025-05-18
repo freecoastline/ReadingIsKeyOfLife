955a6c697b01ddf8fdb5cc5240a48415f5315aea
ken <jitamm20081@gmail.com>
2025-05-18 15:32:30 +0800

    Initial Commit

---
+++ ReadingIsKeyOfLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadingIsKeyOfLife
+//
+//  Created by ken on 2025/5/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
