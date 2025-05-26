a4a1306dd1419e53998381ca3c0cf57a4850786e
ken <jitamm20081@gmail.com>
2025-05-27 00:05:30 +0800

    feat:add tabbar

--- ReadingIsKeyOfLife/ViewController.swift
+++ ReadingIsKeyOfLife/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let bookTab = UITab(title: "Book", image: UIImage.init(systemName: "book.fill"), identifier: "book") { _ in
+            let bookViewController = UITableViewController()
+            bookViewController.view.frame = CGRect(x: 0, y: 0, width: 400, height: 700)
+            bookViewController.view.backgroundColor = .white
+            return bookViewController
+        }
+        let statisticsTab = UITab(title: "statistics", image: UIImage.init(systemName: "receipt"), identifier: "statistics") { _ in
+            let bookViewController = UITableViewController()
+            bookViewController.view.frame = CGRect(x: 0, y: 0, width: 400, height: 700)
+            bookViewController.view.backgroundColor = .white
+            return bookViewController
+        }
+        let tabbarController = UITabBarController(tabs: [bookTab, statisticsTab])
+        self.addChild(tabbarController)
+        self.view.addSubview(tabbarController.view)
     }
-
-
 }
 
