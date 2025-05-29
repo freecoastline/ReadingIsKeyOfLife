163ce9efd554984307931afb24daabca885c744b
ken <jitamm20081@gmail.com>
2025-05-29 23:38:19 +0800

    delete: useless code for iglistkit

--- ReadingIsKeyOfLife/BookViewController.swift
+++ ReadingIsKeyOfLife/BookViewController.swift
@@ -19,32 +19,10 @@ class BookViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        let updater = ListAdapterUpdater()
-        let adapter = ListAdapter(updater: updater, viewController: self)
-        view.addSubview(bookCollectionView)
-        adapter.collectionView = bookCollectionView
-        adapter.dataSource = self
+        super.viewDidLoad()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension BookViewController:ListAdapterDataSource {
-    func objects(for listAdapter: ListAdapter) -> [any ListDiffable] {
-        [1, 1, 1] as [any ListDiffable]
-    }
-    
-    func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
-        if object is String {
-            return BookStringSectionController()
-        } else {
-            return BookNumberSectionController()
-        }
-    }
-    
-    func emptyView(for listAdapter: ListAdapter) -> UIView? {
-        nil
     }
 }
