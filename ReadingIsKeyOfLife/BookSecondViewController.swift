15a66ac4b3756e81d0c7b9fd2277e988d50f8355
ken <jitamm20081@gmail.com>
2025-07-13 22:07:28 +0800

    ajust font according to width

--- ReadingIsKeyOfLife/BookSecondViewController.swift
+++ ReadingIsKeyOfLife/BookSecondViewController.swift
@@ -11,17 +11,17 @@ import UIKit
 class BookSecondViewController:UIViewController {
     lazy var expandLabel:UILabel = {
         let label = UILabel()
-        label.text = "ken"
+        label.text = "kenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenkenenkenkenkenkenkenkenkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenenkenkenkenknkenkenkenkenkenkenkenkenkenkenke"
         label.numberOfLines = 0
         label.adjustsFontSizeToFitWidth = true
-        label.minimumScaleFactor = 0.1
+        label.minimumScaleFactor = 0.5
         return label
     }()
     override func viewDidLoad() {
         view.backgroundColor = .green
         view.addSubview(expandLabel)
         expandLabel.snp.makeConstraints { make in
-            make.height.width.equalTo(200)
+            make.height.width.equalTo(100)
             make.center.equalToSuperview()
         }
     }
